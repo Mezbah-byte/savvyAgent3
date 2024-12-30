5ae3a60e1cf70c4dc6feda0957e11005ed514fc7
Mezbah-byte <mazbahurrahman04@gmail.com>
2024-12-30 13:42:39 +0600

    datatable

--- application/views/dashboard/order_list.php
+++ application/views/dashboard/order_list.php
@@ -14,32 +14,25 @@
 <div class="content-wrapper">
 
   <!-- Content -->
-
   <div class="container-xxl flex-grow-1 container-p-y">
-
 
     <!-- Hoverable Table rows -->
     <div class="card">
       <h5 class="card-header">Course List</h5>
       <div class="table-responsive text-nowrap">
-        <table class="table table-hover">
+        <table id="courseTable" class="table table-hover">
           <thead>
             <tr>
               <th>User Image</th>
               <th>User Name</th>
               <th>Phone Number</th>
               <th>Course Name</th>
-
-
               <th>Course Type</th>
               <th>Course Price</th>
               <th>Course Quantity</th>
               <th>Total Amount</th>
-
               <th>Requested at</th>
               <th>Worked at</th>
-
-
               <th>Payment Method</th>
               <th>TRX</th>
               <th>Screen Shot</th>
@@ -50,26 +43,23 @@
 
             <?php foreach ($datas as $data) { ?>
               <tr>
-
-                <td><img src="<?php echo $data['userImg']; ?>" class="rounded-circle" height="50px" width="50px" /> <span
-                    class="fw-medium"><?php echo $data['name'] ?></span></td>
+                <td>
+                  <img src="<?php echo $data['userImg']; ?>" class="rounded-circle" height="50px" width="50px" />
+                  <span class="fw-medium"><?php echo $data['name'] ?></span>
+                </td>
                 <td><?php echo $data['name'] . ' (' . $data['username'] . ')'; ?></td>
                 <td><?php echo $data['phoneNumber']; ?></td>
                 <td><?php echo $data['courseName']; ?></td>
-
-
                 <td><?php echo $data['courseType']; ?></td>
                 <td><?php echo $data['amount']; ?></td>
                 <td><?php echo $data['quantity']; ?></td>
                 <td><?php echo $data['amount'] * $data['quantity']; ?></td>
-
-
                 <td><?php echo $data['created_at']; ?></td>
                 <td><?php echo $data['updated_at']; ?></td>
-
-
-                <td><img src="<?php echo $data['paymentMethodIcon']; ?>" height="30px"
-                    width="30px" /><?php echo $data['paymentMethod']; ?></td>
+                <td>
+                  <img src="<?php echo $data['paymentMethodIcon']; ?>" height="30px" width="30px" />
+                  <?php echo $data['paymentMethod']; ?>
+                </td>
                 <td><?php echo $data['trx']; ?></td>
                 <td>
                   <img src="<?php echo $data['ss']; ?>" height="50px" width="50px"
@@ -77,13 +67,16 @@
                 </td>
                 <td>
                   <div class="dropdown">
-                    <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i
-                        class="bx bx-dots-vertical-rounded"></i></button>
+                    <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
+                      <i class="bx bx-dots-vertical-rounded"></i>
+                    </button>
                     <div class="dropdown-menu">
-                      <a class="dropdown-item" href="<?php echo base_url() . 'acceptOrder/' . $data['id'] ?>"><i
-                          class="bx bx-edit-alt me-1"></i> Accept</a>
-                      <a class="dropdown-item" href="<?php echo base_url() . 'cancelOrder/' . $data['id'] ?>"><i
-                          class="bx bx-trash me-1"></i> Cancel</a>
+                      <a class="dropdown-item" href="<?php echo base_url() . 'acceptOrder/' . $data['id'] ?>">
+                        <i class="bx bx-edit-alt me-1"></i> Accept
+                      </a>
+                      <a class="dropdown-item" href="<?php echo base_url() . 'cancelOrder/' . $data['id'] ?>">
+                        <i class="bx bx-trash me-1"></i> Cancel
+                      </a>
                     </div>
                   </div>
                 </td>
@@ -96,11 +89,8 @@
     </div>
     <!--/ Hoverable Table rows -->
 
-
-
   </div>
   <!-- / Content -->
-
 
   <!-- Modal Structure -->
   <div id="imageModal"
@@ -118,18 +108,38 @@
   <!-- JavaScript -->
   <script>
     function showImageModal(imageSrc) {
-      // Set the modal image source
       document.getElementById('modalImage').src = imageSrc;
-      // Display the modal
       document.getElementById('imageModal').style.display = 'block';
     }
 
     function closeModal() {
-      // Hide the modal
       document.getElementById('imageModal').style.display = 'none';
     }
   </script>
 
+  <!-- Include DataTables.js -->
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.min.css" />
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+
+  <!-- Initialize DataTables -->
+  <script>
+    $(document).ready(function () {
+      $('#courseTable').DataTable({
+        paging: true,
+        searching: true,
+        ordering: true,
+        lengthChange: true,
+        pageLength: 10,
+        responsive: true,
+        language: {
+          search: "_INPUT_",
+          searchPlaceholder: "Search courses..."
+        }
+      });
+    });
+  </script>
+</div>
 
 
 
@@ -152,4 +162,6 @@
 
 
 
-  <?php include('footer.php') ?>
+
+
+<?php include('footer.php') ?>
