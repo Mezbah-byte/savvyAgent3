163946c1202c8e0e1cd113369ab5d9b0d62694f6
Mezbah-byte <mazbahurrahman04@gmail.com>
2025-03-04 08:37:40 +0600

    bug fix

--- application/controllers/Course.php
+++ application/controllers/Course.php
@@ -188,7 +188,7 @@ class Course extends CI_Controller
 
                         if($courseDetails['type'] == "premium"){
                             $bincome = array();
-                            $bincome['from_user'] = $customerDetails['user_un_id'];
+                            $bincome['from_user'] = $customerDetails['un_id'];
                             $fbincomeorm['to_user'] = $userDetails['un_id'];
                             $bincome['amount'] = $iBonus;
                             $bincome['package_id'] = $getCourseOrderDetails['course_id'];
