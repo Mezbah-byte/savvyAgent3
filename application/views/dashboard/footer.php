4a522736d383be9aa1efe4c7fc1b2ef8dac07b2a
Mezbah-byte <mazbahurrahman04@gmail.com>
2025-05-10 16:20:05 +0600

    some fix

--- application/views/dashboard/footer.php
+++ application/views/dashboard/footer.php
@@ -6,18 +6,6 @@
       <script>document.write(new Date().getFullYear())</script>, made with ❤️ by <a href="https://mysavvybd.com/"
         target="_blank" class="footer-link fw-medium">Savvy IT team</a>
     </div>
-    <div class="d-none d-lg-inline-block">
-
-      <a href="https://themeforest.net/licenses/standard" class="footer-link me-4" target="_blank">License</a>
-      <a href="https://1.envato.market/pixinvent_portfolio" target="_blank" class="footer-link me-4">More Themes</a>
-
-      <a href="https://demos.pixinvent.com/frest-html-admin-template/documentation/" target="_blank"
-        class="footer-link me-4">Documentation</a>
-
-
-      <a href="https://pixinvent.ticksy.com/" target="_blank" class="footer-link d-none d-sm-inline-block">Support</a>
-
-    </div>
   </div>
 </footer>
 <!-- / Footer -->
