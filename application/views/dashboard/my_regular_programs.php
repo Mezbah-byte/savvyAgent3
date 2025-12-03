9c154b740980c9a50e5f6c788ee36f873dae715b
mezbah <mazbahurrahman04@gmail.com>
2025-12-03 23:00:11 +0600

    asdfg

--- application/views/dashboard/my_regular_programs.php
+++ application/views/dashboard/my_regular_programs.php
@@ -69,7 +69,7 @@
                 </tr>
               <?php } else { ?>
                 <?php
-                $n = 0;
+                $n = 1;
                    foreach ($myPrograms as $prog) { ?>
                   <tr>
                     <td><?= $n++ ?></td>
